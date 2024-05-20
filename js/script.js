9730824fc16d6ff25e1e7531955c99a3bc29fe8b
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-20 19:44:34 +0200

    aggiungo if else

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
 const gridElement = document.querySelector("div#grid");
 
-for (let index = 0; index < 100; index++) {
-    const squareElement = document.createElement("article");
-
-    squareElement.classList.add("square");
-
-    squareElement.append(index +1);
-    squareElement.addEventListener("click", function(){
-        squareElement.classList.add("active");
-    });
-
-    gridElement.appendChild(squareElement);
-}
-
 function generateBombs(max) {
     const bombs = [];
     while (bombs.length < 16) {
@@ -26,4 +13,24 @@ function generateBombs(max) {
 
 const gridSize = 100;
 const bombs = generateBombs(gridSize);
-console.log("bombs", bombs);
+
+const playButton = document.getElementById("play-button");
+playButton.addEventListener("click", function(){
+    gridElement.innerHTML = '';
+        for (let index = 0; index < gridSize; index++) {
+        const squareElement = document.createElement("article");
+        squareElement.classList.add("square");
+        squareElement.append(index + 1);
+
+        squareElement.addEventListener("click", function() {
+            if (bombs.includes(index + 1)) {
+                squareElement.classList.add("bomb");
+                alert("Hai perso! Hai calpestato una bomba! Partita terminata.");
+            } else {
+                squareElement.classList.add("salvo");
+            }
+        });
+        gridElement.appendChild(squareElement);
+    }
+});
+
